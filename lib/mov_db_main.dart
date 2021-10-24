d4a7eeb3b0b0ca3b21900d0f674d7c5b532c1b02
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2021-10-25 04:13:25 +0700

    making list of Now Playing

--- lib/mov_db_main.dart
+++ lib/mov_db_main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:my_moviepro/widgets/now_play.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+
 
 import 'apps_drawer.dart';
 
@@ -28,6 +31,41 @@ class MoviedbMain extends StatefulWidget{
 }
 
 class _MoviedbMain extends State<MoviedbMain>{
+
+  List nowplayingmovies = [];
+  List topratedmovies = [];
+  List popularmovies = [];
+  List upcomingmovies = [];
+
+  final String apikey = 'f0e9e0e9f224c4feb567ad64ae5f0d04';
+  final apireadacesstoken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGU5ZTBlOWYyMjRjNGZlYjU2N2FkNjRhZTVmMGQwNCIsInN1YiI6IjYxNTM3YThkZThhM2UxMDA4YWU0NTgyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.75AtCGmeKXpaI3QPAlXwlrbWuahAhpr2fu-M_RkIf3Y';
+
+  @override
+  void initState(){
+    loadmovies();
+    super.initState();
+  }
+
+  loadmovies()async{
+    TMDB tmdbCustomLogs = TMDB(ApiKeys(apikey, apireadacesstoken), logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+
+    Map nowplaymovieresult = await tmdbCustomLogs.v3.movies.getNowPlaying();
+    Map topratedmovieresult = await tmdbCustomLogs.v3.movies.getTopRated();
+    Map popularmovieresult = await tmdbCustomLogs.v3.movies.getPouplar();
+    Map upcomingmovieresult = await tmdbCustomLogs.v3.movies.getUpcoming();
+
+    setState((){
+      nowplayingmovies = nowplaymovieresult['results'];
+      topratedmovies = topratedmovieresult['results'];
+      popularmovies = popularmovieresult['results'];
+      upcomingmovies = upcomingmovieresult['results'];
+    });
+
+    print(nowplayingmovies);
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -44,6 +82,11 @@ class _MoviedbMain extends State<MoviedbMain>{
         ],
       ),
       drawer: AppsDrawer(),
+      body: ListView(
+        children: [
+          NowPlaying(nowplay:nowplayingmovies)
+        ],
+      ),
     );
   }
 
---
+++ lib/widgets/now_play.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:my_moviepro/utils/text.dart';
+
+
+class NowPlaying extends StatelessWidget{
+
+  final List nowplay;
+
+  const NowPlaying({Key? key, required this.nowplay}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          modifiedText(text:'Now Playing', size: 26, color: Colors.black,),
+          Padding(
+            padding: EdgeInsets.only(top: 15.0),
+            child: Container(
+              height: 290,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: nowplay.length,
+                itemBuilder: (context, index){
+                  return InkWell(
+                    onTap: (){},
+                    child: Container(
+                      width: 140,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage('https://image.tmdb.org/t/p/w500/'+nowplay[index]['poster_path']),
+                              ),
+                            ),
+                          ),
+                          Container(
+                              child: modifiedText(text: nowplay[index]['title']!=null?nowplay[index]['title']:'Loading..', size: 17, color: Colors.black,)
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+}
